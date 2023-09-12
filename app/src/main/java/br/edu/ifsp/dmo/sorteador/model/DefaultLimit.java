a8382474658e8322ff63782997c8a3102590c02b
Lucas <lucasalexandre00@hotmail.com>
2023-09-12 11:33:43 -0300

    primeiro commit

---
+++ app/src/main/java/br/edu/ifsp/dmo/sorteador/model/DefaultLimit.java
@@ -0,0 +1,29 @@
+package br.edu.ifsp.dmo.sorteador.model;
+
+public class DefaultLimit extends SorteioStrategy{
+    private final int BORDER_DEFAULT = 1000;
+    private static DefaultLimit instance;
+
+    private DefaultLimit(){}
+
+    public static DefaultLimit getInstance(){
+        if (instance == null){
+            instance = new DefaultLimit();
+        }
+        return instance;
+    }
+    @Override
+    public int nextNumber() {
+        return mRandom.nextInt(BORDER_DEFAULT) + 1;
+    }
+
+    @Override
+    public int getLowBorder() {
+        return 1;
+    }
+
+    @Override
+    public int getHighBorder() {
+        return BORDER_DEFAULT;
+    }
+}
